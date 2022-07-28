7c08335eece3c26b110d4a7c316b8c36a702741c
user <user@users-MacBook-Pro.local>
2022-07-28 15:51:03 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div>
+    <input class = 'search-bar'type = 'text' placeholder = "Search"/>
+    <br>
+    </br>
+    <br></br>
+    <img class = "thumbnail" draggable = "false" alt = "Rick Astley" width = "360" src = "https://i.ytimg.com/vi/dQw4w9WgXcQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDRxusbm2_TGTnDWEIhBTYW2cUQkw/path"/>
+    </div>
+    <p class = "video-title">
+    Rick Astley - Never Gonna Give You Up (Official Music Video)  </p>
+    <p class = "video-stats" >
+    1,256,456,153 views &#183; 25 Oct 2009 </p>
+    <p class = "video-author" >
+      Rick Astley &#10003;
+    </p>
+    <p class = "video-description" >
+    The official video for “Never Gonna Give You Up” by Rick Astley
+    </p>
+    <p class = "apple-text">
+     Shop early for the best selection of holiday favourites.<span class = "shop-link"> Shop now &#62; </span>
+    </p>
+    Shop early for the <strong class = ''> best selection</strong> of <u>holiday</u> favourites.
+     <span class = "span-example" >Shop now </span>
+    <button class = "subscribe-button" >
+     SUBSCRIBE </button>
+    <button class = "join-button" >
+      Join my channel </button>
+    <button class = "tweet-button" >
+      Tweet
+    </button>
+    <a href = "https://onlyfans.com/onlyfans" target = "_blank" > Link to OnlyFans</a>
+    </>
+  );
+}
+
+export default App;
